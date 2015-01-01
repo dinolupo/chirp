c8a0c5c67bf87ef2e753c691b76c906622f244dc
antdimot <antdimot@mactony.homenet.telecomitalia.it>
2015-01-01 13:06:03 +0100

    add authentication

--- app/client/js/controllers/homeController.js
+++ app/client/js/controllers/homeController.js
@@ -1,12 +1,16 @@
 
 
-appControllers.controller('homeCtrl', ['$scope', '$log', '$http', 'config',
-    function($scope, $log, $http, config) {
+appControllers.controller('homeCtrl', ['$scope', '$log', '$http', '$rootScope', 'config',
+    function($scope, $log, $http, $rootScope, config) {
+        var url = config.api+ "/home/" + $rootScope.connectedUser.username;
 
-        $http.get( config.api+ "/home/antdimot").success( function(data) {
-            $log.debug(data);
-
-            $scope.chirps = data;
-        });
+        $http.get(url).
+            success( function(data) {
+                $log.debug(data);
+                $scope.chirps = data;
+            }).
+            error( function(data) {
+                $log.error('Error on home request');
+            });
     }
 ]);
--- app/client/js/controllers/publicController.js
+++ app/client/js/controllers/publicController.js
@@ -1,6 +1,23 @@
 
-appControllers.controller('publicCtrl', ['$scope', '$log', '$http', 'config',
-    function($scope, $log, $http, config) {
+appControllers.controller('publicCtrl', ['$scope', '$log', '$http', '$location', '$rootScope', 'config',
+    function($scope, $log, $http, $location, $rootScope, config) {
+
+        $scope.authenticate = function() {
+            var url = config.api+ "/authenticate";
+            var username = $scope.loginform.username;
+            var password = $scope.loginform.password;
+
+            $http.post( url, { username: username, password: password }).
+                success(function(data) {
+                    $log.debug('success on post request');
+                    $rootScope.connectedUser = data;
+                    $location.path('/home');
+                }).
+                error(function(data){
+                    $log.error('error on post request');
+                    alert('Credentials are not valid!')
+                });
+        };
 
         $http.get( config.api+ "/public").success( function(data) {
             $log.debug(data);
