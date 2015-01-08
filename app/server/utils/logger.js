457319ecefdf7fcdbde8763d0cf40d217eaa9702
antdimot <antonio.dimotta@gmail.com>
2015-01-08 11:17:53 +0100

    update

--- app/server/utils/logger.js
+++ app/server/utils/logger.js
@@ -6,7 +6,7 @@ var logger = new winston.Logger({
     transports: [
         new winston.transports.File({
             level: 'info',
-            filename: './logs/chirp-all-logs.log',
+            filename: './log/chirp-all-logs.log',
             handleExceptions: true,
             json: true,
             maxsize: 5242880, //5MB
