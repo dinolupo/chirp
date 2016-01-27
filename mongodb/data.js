0d530d1eaf90ed3b2e25415642b9133d6405195b
antdimot <antonio.dimotta@gmail.com>
2016-01-27 23:48:30 +0100

    update data.js

--- mongodb/data.js
+++ mongodb/data.js
@@ -9,58 +9,60 @@ var ObjectId = mongodb.ObjectID;
 
 mongoClient.connect(config.mongodb.connectionString, { db: { bufferMaxEntries: 0 } },(err,db)=> {
 
-var user1Id = ObjectId().toString();
-var user2Id = ObjectId().toString();
-var user3Id = ObjectId().toString();
-var post1Id = ObjectId().toString();
-var post2Id = ObjectId().toString();
-var image1Id = ObjectId().toString();
+  var user1Id = ObjectId().toString();
+  var user2Id = ObjectId().toString();
+  var user3Id = ObjectId().toString();
+  var post1Id = ObjectId().toString();
+  var post2Id = ObjectId().toString();
 
-var clearData = ()=> {
-    db.dropCollection('users');
-    db.dropCollection('posts');
-    db.dropCollection('images');
-};
+  var clearData = ()=> {
+    return new Promise((resolve,reject)=>{
+      db.dropCollection('users');
+      db.dropCollection('posts');
+      resolve();
+    });
+  };
 
-var addUsers = ()=> {
-    db.collection('users').insert(
-    [
-        {
-            "_id": user1Id,
-            "username": "dimotta",
-            "displayname": "Antonio Di Motta",
-            "password": "greatpass",
-            "email": "antonio.dimotta@gmail.com",
-            "image":"dimotta.jpg",
-            "following": [user2Id,user3Id],
-            "followers": [user2Id]
-        },
-        {
-            "_id": user2Id,
-            "username": "dinolupo",
-            "displayname": "Dino Lupo",
-            "password": "pass",
-            "email": "dino.lupo@gmail.com",
-            "image":"lupo.jpg",
-            "following": [user1Id],
-            "followers": [user1Id]
-        },
-        {
-            "_id": user3Id,
-            "username": "userdemo1",
-            "displayname": "I'm not a bot :)",
-            "password": "pass",
-            "email": "demo1@dd.dd",
-            "image":"default.png",
-            "following": [user1Id],
-            "followers": [user1Id,user3Id]
-        }
-    ]);
-};
+  var addUsers = ()=> {
+    return new Promise((resolve,reject)=>{
+      resolve(db.collection('users').insert(
+      [
+          {
+              "_id": user1Id,
+              "username": "dimotta",
+              "displayname": "Antonio Di Motta",
+              "password": "greatpass",
+              "email": "antonio.dimotta@gmail.com",
+              "image":"dimotta.jpg",
+              "following": [user2Id,user3Id],
+              "followers": [user2Id]
+          },{
+              "_id": user2Id,
+              "username": "dinolupo",
+              "displayname": "Dino Lupo",
+              "password": "pass",
+              "email": "dino.lupo@gmail.com",
+              "image":"lupo.jpg",
+              "following": [user1Id],
+              "followers": [user1Id]
+          },{
+              "_id": user3Id,
+              "username": "userdemo1",
+              "displayname": "I'm not a bot :)",
+              "password": "pass",
+              "email": "demo1@dd.dd",
+              "image":"default.png",
+              "following": [user1Id],
+              "followers": [user1Id,user3Id]
+          }
+      ]));
+    });
+  };
 
-var addPosts = ()=> {
-    db.collection('posts').insert(
-    [
+  var addPosts = ()=> {
+    return new Promise((resolve,reject)=>{
+      resolve(db.collection('posts').insert(
+      [
         {
             "_id": post1Id,
             "username": "dimotta",
@@ -69,8 +71,7 @@ var addPosts = ()=> {
             "image":"dimotta.jpg",
             "timestamp": new Date("2015-09-13T16:30:00Z").toISOString(),
             "text": "My first post using Chirp."
-        },
-        {
+        },{
             "_id": post2Id,
             "username": "userdemo1",
             "ownerid": user3Id,
@@ -79,30 +80,23 @@ var addPosts = ()=> {
             "timestamp": new Date("2015-09-13T16:35:00Z").toISOString(),
             "text": "Are you ready for production?"
           }
-      ]);
-};
+      ]));
+    });
+  };
 
-var addImages = ()=> {
-    fs.readFile('../wwwroot/images/dimotta.jpg',(err,data)=> {
-      if(err) throw err;
-      db.collection('images').insert(
-      [
-          {
-              "_id": image1Id,
-              "name": "dimotta.jpg",
-              "content": data
-          }
-      ]);
+  co(function *(){
+    yield Promise.resolve(clearData());
+    yield Promise.resolve(addUsers());
+    yield Promise.resolve(addPosts());
+
+    return true;
+  })
+  .then((result)=>{
+    console.log("End with success!");
+    process.exit(0);
+  })
+  .catch((error)=>{
+    console.error(error);
   });
-};
-
-co(function* () {
-  yield Promise.resolve(clearData);
-  yield Promise.resolve(addUsers);
-  yield Promise.resolve(addPosts);
-} ).then(
-  (result)=>{console.log("End with success!")},
-  (err)=>{console.err("Error")}
-);
 
 });
